5465fad02e303cc23c069d1d63c5a227c22226ba
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-07-03 19:44:25 +0800

    Understanding Drawer Navigation

---
+++ app/src/main/java/com/kensbunker/notekeeper/ItemsActivity.kt
@@ -0,0 +1,63 @@
+package com.kensbunker.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_items.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.content_items.*
+
+class ItemsActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_items)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            startActivity(Intent(this, NoteActivity::class.java))
+        }
+
+        listItems.layoutManager = LinearLayoutManager(this)
+        listItems.adapter = NoteRecyclerAdapter(this, DataManager.notes)
+
+        var toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        listItems.adapter?.notifyDataSetChanged()
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.items, menu)
+        return true
+    }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        TODO("Not yet implemented")
+    }
+}
