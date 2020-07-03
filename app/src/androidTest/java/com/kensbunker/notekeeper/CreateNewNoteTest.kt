b6f0156d65b7a09749cd4ea50401526ef9858894
Kenneth Mandawe <kenneth.mandawe@cheetahdigital.com>
2020-07-03 11:49:21 +0800

    Tools and Testing: More Complete Automated UI Tests

--- app/src/androidTest/java/com/kensbunker/notekeeper/CreateNewNoteTest.kt
+++ app/src/androidTest/java/com/kensbunker/notekeeper/CreateNewNoteTest.kt
@@ -1,11 +1,14 @@
 package com.kensbunker.notekeeper
 
-import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.*
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
+import junit.framework.Assert.assertEquals
+import org.hamcrest.CoreMatchers.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -13,17 +16,30 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class CreateNewNoteTest {
 
-    @Rule @JvmField
+    @Rule
+    @JvmField
     val noteListActivity = ActivityTestRule(NoteListActivity::class.java)
 
     @Test
     fun createNewNote() {
+        val course = DataManager.courses["android_async"]
         val noteTitle = "Test note title"
         val noteText = "This is the body of our test note"
 
         onView(withId(R.id.fab)).perform(click())
+
+        onView(withId(R.id.spinnerCourses)).perform(click())
+        onData(allOf(instanceOf(CourseInfo::class.java), equalTo(course))).perform(click())
+
         onView(withId(R.id.textNoteTitle)).perform(typeText(noteTitle))
-        onView(withId(R.id.textNoteText)).perform(typeText(noteText))
+        onView(withId(R.id.textNoteText)).perform(typeText(noteText), closeSoftKeyboard())
+
+        pressBack()
+
+        val newNote = DataManager.notes.last()
+        assertEquals(course, newNote.course)
+        assertEquals(noteTitle, newNote.title)
+        assertEquals(noteText, newNote.text)
 
     }
 }
---
+++ app/src/androidTest/java/com/kensbunker/notekeeper/NextThroughNotesTest.kt
@@ -0,0 +1,45 @@
+package com.kensbunker.notekeeper
+
+import androidx.test.espresso.Espresso.onData
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import org.hamcrest.CoreMatchers.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class NextThroughNotesTest {
+    @Rule
+    @JvmField
+    val noteListActivity = ActivityTestRule(NoteListActivity::class.java)
+
+    @Test
+    fun nextThroughNotes() {
+        onData(
+            allOf(
+                instanceOf(NoteInfo::class.java),
+                equalTo(DataManager.notes[0])
+            )
+        ).perform(click())
+
+        for (index in 0..DataManager.notes.lastIndex) {
+            val note = DataManager.notes[index]
+
+            onView(withId(R.id.spinnerCourses)).check(matches(withSpinnerText(note.course?.title)))
+            onView(withId(R.id.textNoteTitle)).check(matches(withText(note.title)))
+            onView(withId(R.id.textNoteText)).check(matches(withText(note.text)))
+
+            if (index != DataManager.notes.lastIndex) {
+                onView(allOf(withId(R.id.action_next), isEnabled())).perform(click())
+            }
+        }
+
+        onView(withId(R.id.action_next)).check(matches(not(isEnabled())))
+
+    }
+}
